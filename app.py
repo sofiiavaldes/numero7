a5f8dac9be79d35cc9f6ba5d5a69ea74fb5986f2
sofiiavaldes <148466719+sofiiavaldes@users.noreply.github.com>
2023-11-22 21:00:56 -0600

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
-
+import dash_bootstrap_components as dbc
 
 import datetime
 import numpy as np
